64bce1f2b730b5158203ad4f4fd83c85833be204
Robert Snedegar <viking@gmail.com>
2026-04-26 10:08:47 -0700

    Phase 0 cleanup: use external render_test package + real Profiles in render tests

--- pkg/planetgen/render/gasgiant_test.go
+++ pkg/planetgen/render/gasgiant_test.go
@@ -1,26 +1,15 @@
-package render
+package render_test
 
 import (
-	"image/color"
 	"testing"
 
-	planetcolor "github.com/rsned/spacemolt-kb/pkg/planetgen/color"
-	"github.com/rsned/spacemolt-kb/pkg/planetgen/types"
+	"github.com/rsned/spacemolt-kb/pkg/planetgen"
+	"github.com/rsned/spacemolt-kb/pkg/planetgen/render"
 )
 
 func TestRenderGasGiantAllPixelsOpaque(t *testing.T) {
-	prof := &types.PlanetProfile{
-		Type:          "jovian",
-		Renderer:      "gas_giant",
-		BandCount:     26,
-		TurbulenceAmp: 0.012,
-		StormCount:    3,
-		StormSize:     0.25,
-		Palette: []planetcolor.ColorStop{
-			{Position: 0.0, Color: color.RGBA{R: 180, G: 140, B: 100, A: 255}},
-		},
-	}
-	cm := RenderGasGiant(prof, 1234, 64)
+	prof := planetgen.Profiles["jovian"]
+	cm := render.RenderGasGiant(prof, 1234, 64)
 	for face := range len(cm.Faces) {
 		for i, c := range cm.Faces[face] {
 			if c.A != 255 {
@@ -31,18 +20,9 @@ func TestRenderGasGiantAllPixelsOpaque(t *testing.T) {
 }
 
 func TestRenderGasGiantDeterministic(t *testing.T) {
-	prof := &types.PlanetProfile{
-		Type:           "ice_giant",
-		Renderer:       "gas_giant",
-		BandCount:      22,
-		TurbulenceAmp:  0.008,
-		BandBlendWidth: 0.45,
-		Palette: []planetcolor.ColorStop{
-			{Position: 0.0, Color: color.RGBA{R: 100, G: 150, B: 185, A: 255}},
-		},
-	}
-	a := RenderGasGiant(prof, 99, 32)
-	b := RenderGasGiant(prof, 99, 32)
+	prof := planetgen.Profiles["ice_giant"]
+	a := render.RenderGasGiant(prof, 99, 32)
+	b := render.RenderGasGiant(prof, 99, 32)
 	for face := range len(a.Faces) {
 		for i := range a.Faces[face] {
 			if a.Faces[face][i] != b.Faces[face][i] {
@@ -53,19 +33,9 @@ func TestRenderGasGiantDeterministic(t *testing.T) {
 }
 
 func TestRenderGasGiantDeterministicWithStorms(t *testing.T) {
-	prof := &types.PlanetProfile{
-		Type:          "jovian",
-		Renderer:      "gas_giant",
-		BandCount:     26,
-		TurbulenceAmp: 0.012,
-		StormCount:    3,
-		StormSize:     0.25,
-		Palette: []planetcolor.ColorStop{
-			{Position: 0.0, Color: color.RGBA{R: 180, G: 140, B: 100, A: 255}},
-		},
-	}
-	a := RenderGasGiant(prof, 42, 32)
-	b := RenderGasGiant(prof, 42, 32)
+	prof := planetgen.Profiles["jovian"] // StormCount: 3
+	a := render.RenderGasGiant(prof, 42, 32)
+	b := render.RenderGasGiant(prof, 42, 32)
 	for face := range len(a.Faces) {
 		for i := range a.Faces[face] {
 			if a.Faces[face][i] != b.Faces[face][i] {
--- pkg/planetgen/render/rocky_test.go
+++ pkg/planetgen/render/rocky_test.go
@@ -1,30 +1,15 @@
-package render
+package render_test
 
 import (
-	"image/color"
 	"testing"
 
-	planetcolor "github.com/rsned/spacemolt-kb/pkg/planetgen/color"
-	"github.com/rsned/spacemolt-kb/pkg/planetgen/types"
+	"github.com/rsned/spacemolt-kb/pkg/planetgen"
+	"github.com/rsned/spacemolt-kb/pkg/planetgen/render"
 )
 
 func TestRenderRockyAllPixelsOpaque(t *testing.T) {
-	prof := &types.PlanetProfile{
-		Type:             "scorched",
-		Renderer:         "rocky",
-		NoiseOctaves:     8,
-		NoiseLacunarity:  2.0,
-		NoisePersistence: 0.5,
-		NoiseScale:       2.5,
-		CraterCount:      200,
-		CraterMinRadius:  0.005,
-		CraterMaxRadius:  0.08,
-		CraterDepth:      0.25,
-		Palette: []planetcolor.ColorStop{
-			{Position: 0.0, Color: color.RGBA{R: 40, G: 40, B: 40, A: 255}},
-		},
-	}
-	cm := RenderRocky(prof, 1234, 64)
+	prof := planetgen.Profiles["scorched"]
+	cm := render.RenderRocky(prof, 1234, 64)
 	for face := range len(cm.Faces) {
 		for i, c := range cm.Faces[face] {
 			if c.A != 255 {
@@ -35,26 +20,9 @@ func TestRenderRockyAllPixelsOpaque(t *testing.T) {
 }
 
 func TestRenderRockyDeterministic(t *testing.T) {
-	prof := &types.PlanetProfile{
-		Type:             "arid",
-		Renderer:         "rocky",
-		NoiseOctaves:     7,
-		NoiseLacunarity:  2.0,
-		NoisePersistence: 0.5,
-		NoiseScale:       2.5,
-		CraterCount:      80,
-		CraterMinRadius:  0.005,
-		CraterMaxRadius:  0.06,
-		CraterDepth:      0.2,
-		HasPolarCaps:     true,
-		PolarCapSize:     0.18,
-		PolarCapNoise:    0.15,
-		Palette: []planetcolor.ColorStop{
-			{Position: 0.0, Color: color.RGBA{R: 100, G: 60, B: 30, A: 255}},
-		},
-	}
-	a := RenderRocky(prof, 7, 32)
-	b := RenderRocky(prof, 7, 32)
+	prof := planetgen.Profiles["arid"]
+	a := render.RenderRocky(prof, 7, 32)
+	b := render.RenderRocky(prof, 7, 32)
 	for face := range len(a.Faces) {
 		for i := range a.Faces[face] {
 			if a.Faces[face][i] != b.Faces[face][i] {
@@ -65,28 +33,10 @@ func TestRenderRockyDeterministic(t *testing.T) {
 }
 
 func TestRenderRockyWithOceanAndSnow(t *testing.T) {
-	prof := &types.PlanetProfile{
-		Type:             "test_terran",
-		Renderer:         "rocky",
-		NoiseOctaves:     6,
-		NoiseLacunarity:  2.0,
-		NoisePersistence: 0.5,
-		NoiseScale:       2.0,
-		OceanLevel:       0.50,
-		OceanColor:       color.RGBA{R: 30, G: 60, B: 140, A: 255},
-		SnowLine:         0.80,
-		HasPolarCaps:     true,
-		PolarCapSize:     0.15,
-		Palette: []planetcolor.ColorStop{
-			{Position: 0.0, Color: color.RGBA{R: 20, G: 80, B: 30, A: 255}},
-			{Position: 1.0, Color: color.RGBA{R: 180, G: 175, B: 160, A: 255}},
-		},
-		EquatorialPalette: []planetcolor.ColorStop{
-			{Position: 0.0, Color: color.RGBA{R: 190, G: 170, B: 120, A: 255}},
-			{Position: 1.0, Color: color.RGBA{R: 200, G: 200, B: 195, A: 255}},
-		},
-	}
-	cm := RenderRocky(prof, 42, 32)
+	// Use the real "terran" profile, which exercises ocean, snow, polar caps,
+	// equatorial palette, and polar palette code paths.
+	prof := planetgen.Profiles["terran"]
+	cm := render.RenderRocky(prof, 42, 32)
 	for face := range len(cm.Faces) {
 		for i, c := range cm.Faces[face] {
 			if c.A != 255 {
